8f7330afefe583425085881fa4beec52eefdaea5
zazasj <zazang0503@naver.com>
2023-12-14 15:18:18 +0900

    CICD 1차 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -9,9 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class CICDCont {
-  // http://localhost:9091/cicd?sw=1
   // http://localhost:9091/cicd?sw=-1
   // http://localhost:9091/cicd?sw=0
+  // http://localhost:9091/cicd?sw=1
+  //http://localhost:9091/cicd?sw=2
+  //http://localhost:9091/cicd?sw=3
+  //54.66.10.137:9091/cicd?sw=3
   @GetMapping("/cicd")
   public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
@@ -27,6 +30,9 @@ public class CICDCont {
       log.info(msg);
     } else if (sw == 2) {
       msg = "/cide workflow 테스트";
+      log.info(msg);
+    }else if (sw == 3) {
+      msg = "/cide Github action + EC2 최종 테스트";
       log.info(msg);
     }
     
